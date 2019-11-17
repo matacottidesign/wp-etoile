e871b1d569919915755c891d37d97c5973784e06
Francesco Matacotti <francesco1984.go@gmail.com>
2019-11-17 18:21:50 +0100

    aggiunto MAIN (2 su 2)

--- global-templates/main.php
+++ global-templates/main.php
@@ -8,7 +8,7 @@
                 <?php 
                 $image = get_field('immagine_main');
                 if( !empty( $image ) ): ?>
-                    <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" width="85px" />
+                    <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" width="90px" />
                 <?php endif; ?>
             </div>
             <div class="text-center">
@@ -21,8 +21,35 @@
                     </a>
                 </div>
             </div>
-
             <?php include 'gallery-main.php' ?>
+        </div>
+        </div>
+
+        <!--DICONO DI NOI-->
+        <div class="col-12 col-md-6 list-group list-group-flush">
+            <div class="d-flex justify-content-center align-items-center pb-3">
+                <?php 
+                $image = get_field('immagine2_main');
+                if( !empty( $image ) ): ?>
+                    <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" width="53px" />
+                <?php endif; ?>
+            </div>
+            <div class="text-center">
+                <h2><?php the_field('titolo2_main'); ?></h2>
+                <?php the_field('descrizione2_main'); ?>
+                <div class="pt-3">
+                    <a class="link-collapse list-group-item list-group-item-action bg-light" data-toggle="collapse" href="#collapseExample2"
+                            role="button" aria-expanded="true" aria-controls="collapseExample2">
+                            Vedi tutto >
+                    </a>
+                </div>
+
+                <div class="collapse" id="collapseExample2" data-parent="#accordionExample">
+                    <div class="card card-body text-left">
+                        <?php the_field('premi2_main'); ?>
+                    </div>
+                </div>
+            </div> 
         </div>
 
     </div>
