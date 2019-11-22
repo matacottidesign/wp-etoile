6817b0caf65ab28b3ff2b97d46a5026accb08650
Francesco Matacotti <francesco1984.go@gmail.com>
2019-11-22 16:50:05 +0100

    gestione text-center NON da codice ma da WP

--- global-templates/main.php
+++ global-templates/main.php
@@ -12,10 +12,10 @@
                         <img class="mt-3" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" width="90px" />
                     <?php endif; ?>
                 </div>
-                <div class="text-center">
+                <div>
                     <h2><?php the_field('titolo_main'); ?></h2>
                     <?php the_field('descrizione_main'); ?>
-                    <div class="pt-3">
+                    <div class="pt-3 text-center">
                         <a class="link-collapse list-group-item list-group-item-action bg-light" data-toggle="collapse" href="#collapseExample"
                                 role="button" aria-expanded="true" aria-controls="collapseExample">
                                 Vedi tutto >
@@ -34,13 +34,13 @@
                         <img class="mt-3" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" width="54px" />
                     <?php endif; ?>
                 </div>
-                <div class="text-center">
+                <div>
                     <h2><?php the_field('titolo2_main'); ?></h2>
                     <?php the_field('descrizione2_main'); ?>
-                    <div class="pt-3">
+                    <div class="pt-3 text-center">
                         <a class="link-collapse list-group-item list-group-item-action bg-light" data-toggle="collapse" href="#collapseExample2"
-                                role="button" aria-expanded="true" aria-controls="collapseExample2">
-                                Vedi tutto >
+                            role="button" aria-expanded="true" aria-controls="collapseExample2">
+                            Vedi tutto >
                         </a>
                     </div>
 
