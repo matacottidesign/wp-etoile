e4da4b3a6107cab566ad9ebe94725bbd7979a473
Francesco Matacotti <francesco1984.go@gmail.com>
2019-12-02 15:33:42 +0100

    trovato errore griglia: div div div > main

--- page-templates/expo.php
+++ page-templates/expo.php
@@ -21,12 +21,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="wrapper" id="full-width-page-wrapper">
 
-	<div>
-
-		<div class="row">
-
-			<div class="col-md-12 content-area" id="primary">
-
 				<main class="site-main" id="main" role="main">
 
 					<?php while ( have_posts() ) : the_post(); ?>
@@ -105,12 +99,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 					<?php endwhile; // end of the loop. ?>
 
 				</main><!-- #main -->
-
-			</div><!-- #primary -->
-
-		</div><!-- .row end -->
-
-	</div><!-- #content -->
 
 </div><!-- #full-width-page-wrapper -->
 
